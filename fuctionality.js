1f6a9281bba508aff80ee4cfffad32ed1511cb08
farouqdaif1 <daiffarouq@gmail.com>
2021-10-18 22:21:13 +0200

    Add remove function

--- fuctionality.js
+++ fuctionality.js
@@ -1,36 +1,26 @@
 // Array to store the data objects
 const colliction = [];
-const div = document.querySelector(".collection");
-const Submit = document.querySelector("#do");
-// let i = 0;
-const btnRemove = document.querySelectorAll(".remove");
-
+const div = document.querySelector('.collection');
+const Submit = document.querySelector('#do');
 // function to catch the data from input field
 function Setlocalstorage() {
-  const Author = document.querySelector("#author");
-  const Title = document.querySelector("#title");
-  localStorage.setItem("Authors", Author.value);
-  localStorage.setItem("titles", Title.value);
+  const Author = document.querySelector('#author');
+  const Title = document.querySelector('#title');
+  localStorage.setItem('Authors', Author.value);
+  localStorage.setItem('titles', Title.value);
 }
 // function to but the data inside the Array
 function SetDataColl() {
-  const Aut = localStorage.getItem("Authors");
-  const ti = localStorage.getItem("titles");
+  const Aut = localStorage.getItem('Authors');
+  const ti = localStorage.getItem('titles');
   const data = { name: Aut, book: ti };
   colliction.push(data);
 }
 
-function remove(index) {
-if(colliction.length>0){
-  colliction.splice(index, 1);
-  display();
-}
- 
-}
 // function to display the content
 function display() {
   div.innerHTML = '';
-  for(let i =0; i<colliction.length;i++){
+  for (let i = 0; i < colliction.length; i + 1) {
     div.innerHTML += `
     <div class=data>
     <p>${colliction[i].book}</p>
@@ -39,20 +29,17 @@ function display() {
     <hr>
     </div>`;
   }
-
-  // i += 1;
-  // remove();
 }
+function remove(index) {
+  if (colliction.length > 0) {
+    colliction.splice(index, 1);
+    display();
+  }
+}
+display();
+remove();
 
-display()
-// btn 1
-// btn 2
-// btn 3
-
-// colliction = [{1},{2}]
-
-// btnRemove.addEventListener('click', remove);
-Submit.addEventListener("click", (e) => {
+Submit.addEventListener('click', (e) => {
   e.preventDefault();
   Setlocalstorage();
   SetDataColl();
